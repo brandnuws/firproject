5a90f7ce78d3075ef632be57b06c9df245f8ff4a
brandnuws <58971101+brandnuws@users.noreply.github.com>
2021-09-28 22:33:51 +0900

    index 페이지 추가

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -2,6 +2,7 @@ package com.example.firstproject.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -9,14 +10,31 @@ import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 
-@Controller
 @Slf4j
+@RequiredArgsConstructor //final 필드값을 알아서 가져옴
+@Controller
 public class ArticleController {
 	@Autowired //스프링부트가 미리 생성해놓은 객체를 가져다가 자동 연결!
-	private ArticleRepository articleRepository;
+	
+	//리파지터리 객체 자동 삽입됨! RequiredArgsConstructor
+	private final ArticleRepository articleRepository;
+	
+	@GetMapping("/articles")
+	public String index(Model model) {
+		// 모든 Article을 가져옴
+        // Iterable 인터페이스는 ArrayList의 부모 인터페이스
+		Iterable<Article> articleList = articleRepository.findAll();
+		// 뷰 페이지로 articles 전달!
+		model.addAttribute("articles", articleList);
+		 // 뷰 페이지 설정
+		return "articles/index";
+	}
+	
+	
 	
 	@GetMapping("/articles/new")
 	public String newArticleForm() {
